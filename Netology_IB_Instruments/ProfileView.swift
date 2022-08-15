d061a7a06644a630cfdeb1c807fc8f0b1693e09d
NatashaBosyakova <bosyakova-n@yandex.ru>
2022-08-15 23:29:19 +0300

    Netology_IB_Instruments

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Наталья Босякова on 14.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var dayOfBirth: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var aboutMe: UITextView!
+        
+}
